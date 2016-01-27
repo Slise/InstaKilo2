b905083bf6e940992a72050ad3c8b711566f5404
Benson Huynh <quoc@shaw.ca>
2016-01-27 14:42:05 -0800

    Final version

--- InstaKilo2/ViewController.m
+++ InstaKilo2/ViewController.m
@@ -37,9 +37,9 @@ typedef NS_ENUM(NSUInteger, PhotoFilterType) {
     Photo *image5 = [[Photo alloc] initWithImage:[UIImage imageNamed:@"condo.jpg"] location:@"Downtown Vancouver" subject:@"weekend"];
     Photo *image6 = [[Photo alloc] initWithImage:[UIImage imageNamed:@"christmas.jpg"] location:@"Home" subject:@"holiday"];
     Photo *image7 = [[Photo alloc] initWithImage:[UIImage imageNamed:@"condo.jpg"] location:@"Home" subject:@"interests"];
-    Photo *image8 = [[Photo alloc] initWithImage:[UIImage imageNamed:@"autumn.jpg"] location:@"Vancouver" subject:@"interests"];
-    Photo *image9 = [[Photo alloc] initWithImage:[UIImage imageNamed:@"bcplace.jpg"] location:@"World Picture" subject:@"travel"];
-    Photo *image10 = [[Photo alloc] initWithImage:[UIImage imageNamed:@"luxuryhome.jpg"] location:@"Vancouver" subject:@"interests"];
+    Photo *image8 = [[Photo alloc] initWithImage:[UIImage imageNamed:@"car.jpg"] location:@"Vancouver" subject:@"interests"];
+    Photo *image9 = [[Photo alloc] initWithImage:[UIImage imageNamed:@"park.jpg"] location:@"World Picture" subject:@"travel"];
+    Photo *image10 = [[Photo alloc] initWithImage:[UIImage imageNamed:@"mansion.jpg"] location:@"Vancouver" subject:@"interests"];
     
     self.photoList = [NSMutableArray arrayWithArray:@[image1, image2, image3, image4, image5, image6, image7, image8, image9, image10]];
     
@@ -81,7 +81,6 @@ typedef NS_ENUM(NSUInteger, PhotoFilterType) {
             groupedPhotos[photo.location] = @[photo];
         }
     }
-    
     return groupedPhotos;
 }
 
